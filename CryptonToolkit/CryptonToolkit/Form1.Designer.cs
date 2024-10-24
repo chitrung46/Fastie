183640dd2ff3c963c3c78fa0483bd5dc71df88b4
Nhat Toan <nhattoan664t@gmail.com>
2024-10-24 14:52:55 +0700

    test

---
+++ CryptonToolkit/CryptonToolkit/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace CryptonToolkit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kryptonColorButton1 = new ComponentFactory.Krypton.Toolkit.KryptonColorButton();
+            this.SuspendLayout();
+            // 
+            // kryptonColorButton1
+            // 
+            this.kryptonColorButton1.Location = new System.Drawing.Point(497, 239);
+            this.kryptonColorButton1.Name = "kryptonColorButton1";
+            this.kryptonColorButton1.Size = new System.Drawing.Size(90, 25);
+            this.kryptonColorButton1.TabIndex = 0;
+            this.kryptonColorButton1.Values.Text = "kryptonColorButton1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kryptonColorButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonColorButton kryptonColorButton1;
+    }
+}
+
---
+++ slide1/slide1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace slide1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lnlA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.Color.Blue;
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblOutput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblOutput.ForeColor = System.Drawing.Color.White;
+            this.lblOutput.Location = new System.Drawing.Point(0, 0);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Padding = new System.Windows.Forms.Padding(10);
+            this.lblOutput.Size = new System.Drawing.Size(800, 43);
+            this.lblOutput.TabIndex = 0;
+            this.lblOutput.Text = "ouput";
+            this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblOutput.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lnlA
+            // 
+            this.lnlA.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lnlA.BackColor = System.Drawing.Color.Blue;
+            this.lnlA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lnlA.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lnlA.ForeColor = System.Drawing.Color.White;
+            this.lnlA.Location = new System.Drawing.Point(174, 144);
+            this.lnlA.Name = "lnlA";
+            this.lnlA.Size = new System.Drawing.Size(70, 30);
+            this.lnlA.TabIndex = 1;
+            this.lnlA.Text = "Input a";
+            this.lnlA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lnlA.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblB
+            // 
+            this.lblB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblB.BackColor = System.Drawing.Color.Blue;
+            this.lblB.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblB.ForeColor = System.Drawing.Color.White;
+            this.lblB.Location = new System.Drawing.Point(174, 192);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(70, 30);
+            this.lblB.TabIndex = 2;
+            this.lblB.Text = "Input b";
+            this.lblB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblB.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(264, 148);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(239, 22);
+            this.txtA.TabIndex = 3;
+            this.txtA.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(264, 200);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(239, 22);
+            this.txtB.TabIndex = 4;
+            this.txtB.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnSum
+            // 
+            this.btnSum.BackColor = System.Drawing.Color.Red;
+            this.btnSum.ForeColor = System.Drawing.Color.White;
+            this.btnSum.Location = new System.Drawing.Point(264, 254);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(70, 30);
+            this.btnSum.TabIndex = 5;
+            this.btnSum.Text = "Sum";
+            this.btnSum.UseVisualStyleBackColor = false;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.BackColor = System.Drawing.Color.Red;
+            this.btnMinus.ForeColor = System.Drawing.Color.White;
+            this.btnMinus.Location = new System.Drawing.Point(345, 254);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(70, 30);
+            this.btnMinus.TabIndex = 6;
+            this.btnMinus.Text = "Minus";
+            this.btnMinus.UseVisualStyleBackColor = false;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Red;
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(428, 254);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(70, 30);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(345, 283);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(70, 30);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lnlA);
+            this.Controls.Add(this.lblOutput);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lnlA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
