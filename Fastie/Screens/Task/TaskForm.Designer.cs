ec8b101550601df7bcb949b66b2591026ef556b9
Nhat Toan <nhattoan664t@gmail.com>
2024-10-30 21:10:56 +0700

    delete panel 1

--- Fastie/Screens/Task/TaskForm.Designer.cs
+++ Fastie/Screens/Task/TaskForm.Designer.cs
@@ -160,11 +160,7 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(7, 94);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(928, 440);
-            this.panel1.TabIndex = 2;
+
             // 
             // TaskForm
             // 
@@ -193,5 +189,6 @@
         private GUI.CustomButton btnAdjustTask;
         private GUI.CustomButton btnReportTask;
         private System.Windows.Forms.Panel pnlTask;
+
     }
 }
