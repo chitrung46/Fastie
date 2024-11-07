7de7ce996df2166514531bcc1620f2a290cccae9
Nhat Toan <nhattoan664t@gmail.com>
2024-11-07 16:37:06 +0700

    test

--- Fastie/Components/LayoutTask/LayoutReportTaskForm.Designer.cs
+++ Fastie/Components/LayoutTask/LayoutReportTaskForm.Designer.cs
@@ -33,16 +33,34 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.lblImageName = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btnDetail = new GUI.CustomButton();
             this.panel3 = new System.Windows.Forms.Panel();
             this.lblFileName = new System.Windows.Forms.Label();
             this.panel5 = new System.Windows.Forms.Panel();
             this.lblReportDate = new System.Windows.Forms.Label();
-            this.btnDetail = new GUI.CustomButton();
+            this.lblTaskName = new System.Windows.Forms.Label();
+            this.lblTaskTime = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.lblJobAssigner = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.btnTaskStatus = new GUI.CustomButton();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.btnTaskDetail = new GUI.CustomButton();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.btnReport = new GUI.CustomButton();
             this.panel1.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel11.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblReportContent
@@ -89,6 +107,25 @@
             this.panel2.Size = new System.Drawing.Size(62, 54);
             this.panel2.TabIndex = 10;
             // 
+            // btnDetail
+            // 
+            this.btnDetail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(171)))), ((int)(((byte)(201)))));
+            this.btnDetail.BorderColor = System.Drawing.Color.White;
+            this.btnDetail.BorderRadius = 15;
+            this.btnDetail.BorderSize = 0;
+            this.btnDetail.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDetail.FlatAppearance.BorderSize = 0;
+            this.btnDetail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDetail.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetail.ForeColor = System.Drawing.Color.White;
+            this.btnDetail.Image = global::Fastie.Properties.Resources.icons8_document_24;
+            this.btnDetail.Location = new System.Drawing.Point(3, 6);
+            this.btnDetail.Name = "btnDetail";
+            this.btnDetail.Size = new System.Drawing.Size(48, 40);
+            this.btnDetail.TabIndex = 2;
+            this.btnDetail.UseVisualStyleBackColor = false;
+            this.btnDetail.Click += new System.EventHandler(this.btnDetail_Click);
+            // 
             // panel3
             // 
             this.panel3.Controls.Add(this.lblFileName);
@@ -125,37 +162,155 @@
             this.lblReportDate.TabIndex = 1;
             this.lblReportDate.Text = "Ngày báo cáo";
             // 
-            // btnDetail
+            // lblTaskName
             // 
-            this.btnDetail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(171)))), ((int)(((byte)(201)))));
-            this.btnDetail.BorderColor = System.Drawing.Color.White;
-            this.btnDetail.BorderRadius = 15;
-            this.btnDetail.BorderSize = 0;
-            this.btnDetail.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnDetail.FlatAppearance.BorderSize = 0;
-            this.btnDetail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDetail.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDetail.ForeColor = System.Drawing.Color.White;
-            this.btnDetail.Image = global::Fastie.Properties.Resources.icons8_document_24;
-            this.btnDetail.Location = new System.Drawing.Point(3, 6);
-            this.btnDetail.Name = "btnDetail";
-            this.btnDetail.Size = new System.Drawing.Size(48, 40);
-            this.btnDetail.TabIndex = 2;
-            this.btnDetail.UseVisualStyleBackColor = false;
-            this.btnDetail.Click += new System.EventHandler(this.btnDetail_Click);
+            this.lblTaskName.AutoSize = true;
+            this.lblTaskName.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaskName.Location = new System.Drawing.Point(19, 19);
+            this.lblTaskName.Name = "lblTaskName";
+            this.lblTaskName.Size = new System.Drawing.Size(182, 21);
+            this.lblTaskName.TabIndex = 0;
+            this.lblTaskName.Text = "Sửa chữa bồn vệ sinh";
+            // 
+            // lblTaskTime
+            // 
+            this.lblTaskTime.AutoSize = true;
+            this.lblTaskTime.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaskTime.Location = new System.Drawing.Point(13, 16);
+            this.lblTaskTime.Name = "lblTaskTime";
+            this.lblTaskTime.Size = new System.Drawing.Size(85, 21);
+            this.lblTaskTime.TabIndex = 0;
+            this.lblTaskTime.Text = "9/12/2024";
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.lblJobAssigner);
+            this.panel6.Location = new System.Drawing.Point(607, 100);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(244, 54);
+            this.panel6.TabIndex = 14;
+            // 
+            // lblJobAssigner
+            // 
+            this.lblJobAssigner.AutoSize = true;
+            this.lblJobAssigner.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJobAssigner.Location = new System.Drawing.Point(13, 16);
+            this.lblJobAssigner.Name = "lblJobAssigner";
+            this.lblJobAssigner.Size = new System.Drawing.Size(146, 21);
+            this.lblJobAssigner.TabIndex = 0;
+            this.lblJobAssigner.Text = "Lương Chí Trung";
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.btnTaskStatus);
+            this.panel7.Location = new System.Drawing.Point(438, 100);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(163, 54);
+            this.panel7.TabIndex = 15;
+            // 
+            // btnTaskStatus
+            // 
+            this.btnTaskStatus.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnTaskStatus.BorderColor = System.Drawing.Color.White;
+            this.btnTaskStatus.BorderRadius = 15;
+            this.btnTaskStatus.BorderSize = 0;
+            this.btnTaskStatus.FlatAppearance.BorderSize = 0;
+            this.btnTaskStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTaskStatus.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTaskStatus.ForeColor = System.Drawing.Color.White;
+            this.btnTaskStatus.Location = new System.Drawing.Point(3, 7);
+            this.btnTaskStatus.Name = "btnTaskStatus";
+            this.btnTaskStatus.Size = new System.Drawing.Size(145, 40);
+            this.btnTaskStatus.TabIndex = 0;
+            this.btnTaskStatus.Text = "Đang thực hiện";
+            this.btnTaskStatus.UseVisualStyleBackColor = false;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.btnTaskDetail);
+            this.panel8.Location = new System.Drawing.Point(1003, 100);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(62, 54);
+            this.panel8.TabIndex = 19;
+            // 
+            // btnTaskDetail
+            // 
+            this.btnTaskDetail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(171)))), ((int)(((byte)(201)))));
+            this.btnTaskDetail.BorderColor = System.Drawing.Color.White;
+            this.btnTaskDetail.BorderRadius = 15;
+            this.btnTaskDetail.BorderSize = 0;
+            this.btnTaskDetail.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTaskDetail.FlatAppearance.BorderSize = 0;
+            this.btnTaskDetail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTaskDetail.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTaskDetail.ForeColor = System.Drawing.Color.White;
+            this.btnTaskDetail.Image = global::Fastie.Properties.Resources.icons8_document_24;
+            this.btnTaskDetail.Location = new System.Drawing.Point(3, 6);
+            this.btnTaskDetail.Name = "btnTaskDetail";
+            this.btnTaskDetail.Size = new System.Drawing.Size(48, 40);
+            this.btnTaskDetail.TabIndex = 2;
+            this.btnTaskDetail.UseVisualStyleBackColor = false;
+            // 
+            // panel9
+            // 
+            this.panel9.Controls.Add(this.lblTaskTime);
+            this.panel9.Location = new System.Drawing.Point(276, 100);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(156, 54);
+            this.panel9.TabIndex = 16;
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.lblTaskName);
+            this.panel10.Location = new System.Drawing.Point(-44, 100);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(314, 54);
+            this.panel10.TabIndex = 17;
+            // 
+            // panel11
+            // 
+            this.panel11.Controls.Add(this.btnReport);
+            this.panel11.Location = new System.Drawing.Point(857, 100);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(143, 54);
+            this.panel11.TabIndex = 18;
+            // 
+            // btnReport
+            // 
+            this.btnReport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.btnReport.BorderColor = System.Drawing.Color.White;
+            this.btnReport.BorderRadius = 15;
+            this.btnReport.BorderSize = 0;
+            this.btnReport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReport.FlatAppearance.BorderSize = 0;
+            this.btnReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReport.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReport.ForeColor = System.Drawing.Color.White;
+            this.btnReport.Location = new System.Drawing.Point(3, 7);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(133, 40);
+            this.btnReport.TabIndex = 0;
+            this.btnReport.Text = "Báo cáo";
+            this.btnReport.UseVisualStyleBackColor = false;
             // 
             // LayoutDetailReportTaskForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel11);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel3);
             this.Name = "LayoutDetailReportTaskForm";
-            this.Size = new System.Drawing.Size(1021, 60);
+            this.Size = new System.Drawing.Size(1082, 254);
             this.Load += new System.EventHandler(this.LayoutDetailReportTaskForm_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -166,6 +321,15 @@
             this.panel3.PerformLayout();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.panel10.ResumeLayout(false);
+            this.panel10.PerformLayout();
+            this.panel11.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -182,5 +346,17 @@
         private System.Windows.Forms.Label lblFileName;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Label lblReportDate;
+        private System.Windows.Forms.Label lblTaskName;
+        private System.Windows.Forms.Label lblTaskTime;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label lblJobAssigner;
+        private System.Windows.Forms.Panel panel7;
+        private GUI.CustomButton btnTaskStatus;
+        private System.Windows.Forms.Panel panel8;
+        private GUI.CustomButton btnTaskDetail;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private GUI.CustomButton btnReport;
     }
 }
