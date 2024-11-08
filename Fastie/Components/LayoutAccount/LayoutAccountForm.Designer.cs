0cd86cc9cc9c89174414d642f3eceb7ec9fe6253
Nhat Toan <nhattoan664t@gmail.com>
2024-11-08 12:40:40 +0700

    Account

---
+++ Fastie/Components/LayoutAccount/LayoutAccountForm.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Fastie.Components.LayoutAccount
+{
+    partial class LayoutAccountForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPersonnelName = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblDepartmentName = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblPositionName = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.lblStatusAccount = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblHasAccount = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.btnAdd = new GUI.CustomButton();
+            this.btnEdit = new GUI.CustomButton();
+            this.btnDelete = new GUI.CustomButton();
+            this.btnDetail = new GUI.CustomButton();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblPersonnelName
+            // 
+            this.lblPersonnelName.AutoSize = true;
+            this.lblPersonnelName.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPersonnelName.Location = new System.Drawing.Point(4, 16);
+            this.lblPersonnelName.Name = "lblPersonnelName";
+            this.lblPersonnelName.Size = new System.Drawing.Size(108, 21);
+            this.lblPersonnelName.TabIndex = 0;
+            this.lblPersonnelName.Text = "Tên nhân sự";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lblDepartmentName);
+            this.panel3.Location = new System.Drawing.Point(235, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(191, 54);
+            this.panel3.TabIndex = 13;
+            // 
+            // lblDepartmentName
+            // 
+            this.lblDepartmentName.AutoSize = true;
+            this.lblDepartmentName.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartmentName.Location = new System.Drawing.Point(3, 16);
+            this.lblDepartmentName.Name = "lblDepartmentName";
+            this.lblDepartmentName.Size = new System.Drawing.Size(110, 21);
+            this.lblDepartmentName.TabIndex = 1;
+            this.lblDepartmentName.Text = "Tên bộ phận";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblPersonnelName);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(229, 54);
+            this.panel2.TabIndex = 14;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.lblPositionName);
+            this.panel4.Location = new System.Drawing.Point(432, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(174, 54);
+            this.panel4.TabIndex = 16;
+            // 
+            // lblPositionName
+            // 
+            this.lblPositionName.AutoSize = true;
+            this.lblPositionName.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPositionName.Location = new System.Drawing.Point(13, 16);
+            this.lblPositionName.Name = "lblPositionName";
+            this.lblPositionName.Size = new System.Drawing.Size(107, 21);
+            this.lblPositionName.TabIndex = 0;
+            this.lblPositionName.Text = "Tên chức vụ";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.lblStatusAccount);
+            this.panel5.Location = new System.Drawing.Point(612, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(134, 54);
+            this.panel5.TabIndex = 17;
+            // 
+            // lblStatusAccount
+            // 
+            this.lblStatusAccount.AutoSize = true;
+            this.lblStatusAccount.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatusAccount.Location = new System.Drawing.Point(13, 16);
+            this.lblStatusAccount.Name = "lblStatusAccount";
+            this.lblStatusAccount.Size = new System.Drawing.Size(93, 21);
+            this.lblStatusAccount.TabIndex = 0;
+            this.lblStatusAccount.Text = "Trạng thái";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblHasAccount);
+            this.panel1.Location = new System.Drawing.Point(752, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(122, 54);
+            this.panel1.TabIndex = 18;
+            // 
+            // lblHasAccount
+            // 
+            this.lblHasAccount.AutoSize = true;
+            this.lblHasAccount.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHasAccount.Location = new System.Drawing.Point(13, 16);
+            this.lblHasAccount.Name = "lblHasAccount";
+            this.lblHasAccount.Size = new System.Drawing.Size(88, 21);
+            this.lblHasAccount.TabIndex = 0;
+            this.lblHasAccount.Text = "Tài khoản";
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.btnDetail);
+            this.panel6.Location = new System.Drawing.Point(1032, 3);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(81, 54);
+            this.panel6.TabIndex = 20;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.btnAdd);
+            this.panel7.Controls.Add(this.btnEdit);
+            this.panel7.Controls.Add(this.btnDelete);
+            this.panel7.Location = new System.Drawing.Point(880, 3);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(146, 54);
+            this.panel7.TabIndex = 21;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.btnAdd.BorderColor = System.Drawing.Color.White;
+            this.btnAdd.BorderRadius = 15;
+            this.btnAdd.BorderSize = 0;
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Image = global::Fastie.Properties.Resources.icons8_plus_23;
+            this.btnAdd.Location = new System.Drawing.Point(7, 7);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(40, 40);
+            this.btnAdd.TabIndex = 26;
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.btnEdit.BorderColor = System.Drawing.Color.White;
+            this.btnEdit.BorderRadius = 15;
+            this.btnEdit.BorderSize = 0;
+            this.btnEdit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEdit.FlatAppearance.BorderSize = 0;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.Image = global::Fastie.Properties.Resources.icons8_pencil_23;
+            this.btnEdit.Location = new System.Drawing.Point(53, 7);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(40, 40);
+            this.btnEdit.TabIndex = 25;
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.btnDelete.BorderColor = System.Drawing.Color.White;
+            this.btnDelete.BorderRadius = 15;
+            this.btnDelete.BorderSize = 0;
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Image = global::Fastie.Properties.Resources.icons8_delete_23;
+            this.btnDelete.Location = new System.Drawing.Point(99, 7);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(40, 40);
+            this.btnDelete.TabIndex = 24;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnDetail
+            // 
+            this.btnDetail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(171)))), ((int)(((byte)(201)))));
+            this.btnDetail.BorderColor = System.Drawing.Color.White;
+            this.btnDetail.BorderRadius = 15;
+            this.btnDetail.BorderSize = 0;
+            this.btnDetail.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDetail.FlatAppearance.BorderSize = 0;
+            this.btnDetail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDetail.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetail.ForeColor = System.Drawing.Color.White;
+            this.btnDetail.Image = global::Fastie.Properties.Resources.icons8_document_24;
+            this.btnDetail.Location = new System.Drawing.Point(11, 6);
+            this.btnDetail.Name = "btnDetail";
+            this.btnDetail.Size = new System.Drawing.Size(40, 40);
+            this.btnDetail.TabIndex = 1;
+            this.btnDetail.UseVisualStyleBackColor = false;
+            // 
+            // LayoutAccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
+            this.Name = "LayoutAccountForm";
+            this.Size = new System.Drawing.Size(1117, 60);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPersonnelName;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblDepartmentName;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lblPositionName;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label lblStatusAccount;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblHasAccount;
+        private System.Windows.Forms.Panel panel6;
+        private GUI.CustomButton btnDetail;
+        private System.Windows.Forms.Panel panel7;
+        private GUI.CustomButton btnAdd;
+        private GUI.CustomButton btnEdit;
+        private GUI.CustomButton btnDelete;
+    }
+}
--- Fastie/Screens/Account/AccountForm.cs
+++ Fastie/Screens/Account/AccountForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DTO;
+using Fastie.Components.LayoutAccount;
+using Fastie.Components.LayoutDecentralization;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,28 @@ namespace Fastie
         public AccountForm()
         {
             InitializeComponent();
+        }
+
+        private void loadDataAccount()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var layoutAccountForm = new LayoutAccountForm(this)
+                {
+                    PersonnelName = "Nhật Toàn",
+                    PositionName = "Quản lý",
+                    DepartmentName = "Kế toán",
+                    StatusAccount = "Hoạt động",
+                    HasAccount = "Đã có",
+                    IdAccount = "ACC" + i
+                };
+                flowLayoutPanelAcoount.Controls.Add(layoutAccountForm);
+            }
+        }
+
+        private void AccountForm_Load(object sender, EventArgs e)
+        {
+            loadDataAccount();
         }
     }
 }
